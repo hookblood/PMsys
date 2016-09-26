ea3f8931aebb6d5892a9c3a23289188ea1d2115c
hookblood <512457334@qq.com>
2016-09-26 21:55:02 +0800

    11

---
+++ Project-platform/web/Think/Library/Gjc/YarClient.class.php
@@ -0,0 +1,34 @@
+<?php
+namespace Gjc;
+/**
+ * 模块名称：服务类
+ * 开发人员：shijunli
+ * 开发日期：2016-6-1 10:00:21
+ */
+class YarClient {
+    /**
+     * 初始化服务接口
+     * @param string $m 模块名
+     * @param string $c 控制器
+     * @return Ambigous <NULL, \Yar_client>
+     */
+    public static function &get_instance($m='',$c='') {
+        // 获取配置信息
+        $ServiceUrl = C('ServiceUrl');
+        if(!empty($m)||!empty($c)){
+            $ServiceUrl = $ServiceUrl . '/' . $m . '/'. $c;
+        }
+        static $obj = null;
+        if ($obj == null) {
+            $obj = new \Yar_client($ServiceUrl);
+        }
+        return $obj;
+    }
+    
+    function __construct() {
+        $this->init ();
+    }
+    function init() {
+       
+    }
+}

1c73d11e6ecc4b5fb5c5487a2519470ade8f6d54
hookblood <512457334@qq.com>
2016-09-28 09:29:12 +0800

    商务团队（还差接口数据）

--- Project-platform/web/Apps/Home/Controller/BussinessController.class.php
+++ Project-platform/web/Apps/Home/Controller/BussinessController.class.php
@@ -17,25 +17,62 @@ class BussinessController extends BaseController {
     }
 	/**
 	 * 获取商务经理列表
-	 * 
+	 * @params:
+     *
 	 */
     public function get_pmlist(){
-        //获取
+        //从接口获取数据
         $list = $this->Yar->managerlist_Interface(1);
         $this->assign('list',$list);
-//        var_dump($list);
         $this->display('bussiness/bussiness_list');
         }
     /**
      * 获取商务经理的项目列表
-     *
+     * @params:
+     * $pmid  商务经理id
      */
     public function get_projectlist($pmid){
-        //获取
         $list = $this->Business_Yar->get_projectlist($pmid);
         $this->assign('list',$list);
         $this->assign('uid',$_COOKIE['user_id']);
-//        var_dump($list);
         $this->display('bussiness/project_list');
     }
+    /**
+ * 获取商务项目详情
+ * @params:
+ * $jid  项目id
+ */
+    public function detail($jid){
+        $list = $this->Business_Yar->get_projectdetail($jid);
+        $this->assign('list',$list);
+        $this->display('bussiness/bussiness_detail');
     }
+    /**
+     * 增加商务跟进情况
+     * @params:
+     * $id 发布人ID
+     * $jid 项目id
+     * $content 跟进内容
+     * $method 解决方案
+     */
+    public function addcomment($id,$jid,$content,$method){
+        $list = $this->Business_Yar->addcommentC($id,$jid,$content,$method);
+    }
+
+    /**
+     * 删除项目
+     * @params:
+     * $id 项目ID
+     */
+    public function delproject(){
+        $id=$_GET['id'];
+        $pmid=$_GET['uid'];
+//        var_dump($id);die();
+        $list =  $this->Business_Yar->delprojectC($id);
+        echo "<script LANGUAGE='JavaScript'>";
+        echo "location.href='index.php?s=/Home/Bussiness/get_projectlist&pmid=$pmid'";
+        echo "</script>";
+//        $this->display('bussiness/project_list');
+    }
+
+}
